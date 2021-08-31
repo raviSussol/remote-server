3bb7f0b723497357ea4dc5af7e9059b36c283882
Clemens Zeidler <clemens@msupply.foundation>
2021-08-31 16:21:30 +1200

    Add MyConnection type

--- src/database/repository/diesel/item_line.rs
+++ src/database/repository/diesel/item_line.rs
@@ -5,24 +5,46 @@ use crate::database::{
     schema::ItemLineRow,
 };
 
+use diesel::backend::{Backend, SupportsDefaultKeyword, UsesAnsiSavepointSyntax};
+use diesel::connection::{AnsiTransactionManager, TransactionManager};
+use diesel::deserialize::FromSql;
+use diesel::query_builder::bind_collector::RawBytesBindCollector;
+use diesel::query_builder::{BindCollector, QueryBuilder};
+use diesel::sql_types::TypeMetadata;
+use diesel::Connection;
 use diesel::{
     prelude::*,
     r2d2::{ConnectionManager, Pool},
 };
 
-#[derive(Clone)]
-pub struct ItemLineRepository {
-    pool: Pool<ConnectionManager<DBBackendConnection>>,
+use byteorder::NativeEndian;
+
+trait MyConnection: Connection
+where
+    Self::Backend: Backend,
+{
 }
 
-impl ItemLineRepository {
-    pub fn new(pool: Pool<ConnectionManager<DBBackendConnection>>) -> Self {
+impl<T> MyConnection for T
+where
+    T: Connection,
+    T::Backend: Backend,
+{
+}
+
+#[derive(Clone)]
+pub struct ItemLineRepository<T: 'static + MyConnection> {
+    pool: Pool<ConnectionManager<T>>,
+}
+
+impl<T: MyConnection> ItemLineRepository<T> {
+    pub fn new(pool: Pool<ConnectionManager<T>>) -> Self {
         ItemLineRepository { pool }
     }
 
     pub async fn insert_one(&self, item_line_row: &ItemLineRow) -> Result<(), RepositoryError> {
         use crate::database::schema::diesel_schema::item_line::dsl::*;
-        let connection = get_connection(&self.pool)?;
+        let connection = self.pool.get().unwrap();
         diesel::insert_into(item_line)
             .values(item_line_row)
             .execute(&connection)?;
@@ -31,7 +53,7 @@ impl ItemLineRepository {
 
     pub async fn find_one_by_id(&self, item_line_id: &str) -> Result<ItemLineRow, RepositoryError> {
         use crate::database::schema::diesel_schema::item_line::dsl::*;
-        let connection = get_connection(&self.pool)?;
+        let connection = self.pool.get().unwrap();
         let result = item_line.filter(id.eq(item_line_id)).first(&connection)?;
         Ok(result)
     }
