e802b0ad3a2b766b4b5488e5ff5dffd61707a924
andreievg <andrei.evguenov@gmail.com>
2021-09-07 01:28:11 +1200

    example of transaction with existing repository

--- src/database/repository/diesel/item.rs
+++ src/database/repository/diesel/item.rs
@@ -2,13 +2,43 @@ use super::DBBackendConnection;
 
 use crate::database::{
     repository::{repository::get_connection, RepositoryError},
-    schema::ItemRow,
+    schema::{ItemRow, ItemRowType},
 };
 
 use diesel::{
     prelude::*,
     r2d2::{ConnectionManager, Pool},
 };
+use r2d2::PooledConnection;
+
+pub fn example(pool: &Pool<ConnectionManager<DBBackendConnection>>) -> Result<(), RepositoryError> {
+    let connection = &get_connection(pool)?;
+    connection
+        .transaction::<(), RepositoryError, _>(|| {
+            let row = ItemRow {
+                id: "abc".to_owned(),
+                item_name: "amox".to_owned(),
+                type_of: ItemRowType::General,
+            };
+
+            ItemRepository::insert_one_sync(&row, connection)?;
+
+            let row = ItemRow {
+                id: "abc".to_owned(),
+                item_name: "amox".to_owned(),
+                type_of: ItemRowType::General,
+            };
+
+            ItemRepository::insert_one_sync(&row, connection)?;
+
+            // etc ofcourse above would be similar to import_sync_records, but no need for async etc..
+
+            Ok(())
+        })
+        .unwrap();
+
+    Ok(())
+}
 
 #[derive(Clone)]
 pub struct ItemRepository {
@@ -17,15 +47,21 @@ pub struct ItemRepository {
 
 impl ItemRepository {
     pub fn new(pool: Pool<ConnectionManager<DBBackendConnection>>) -> ItemRepository {
-        ItemRepository { pool }
+        ItemRepository { pool: pool }
     }
 
     pub async fn insert_one(&self, item_row: &ItemRow) -> Result<(), RepositoryError> {
+        ItemRepository::insert_one_sync(item_row, &get_connection(&self.pool)?)
+    }
+
+    pub fn insert_one_sync(
+        item_row: &ItemRow,
+        pool: &PooledConnection<ConnectionManager<DBBackendConnection>>,
+    ) -> Result<(), RepositoryError> {
         use crate::database::schema::diesel_schema::item::dsl::*;
-        let connection = get_connection(&self.pool)?;
-        diesel::insert_into(item)
-            .values(item_row)
-            .execute(&connection)?;
+
+        diesel::insert_into(item).values(item_row).execute(pool)?;
+
         Ok(())
     }
 
