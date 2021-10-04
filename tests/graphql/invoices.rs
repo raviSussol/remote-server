d499e3fc9e4a0880f0e48f46beb031b3f74d4e0f
wlthomson <will.tho@outlook.com>
2021-10-04 13:19:43 +1300

    Calculate expected invoices gql result from mock data
    Removes hardcoded expected gql result from invoices test. Also some
    minor tidy-ups and formatting nits.

--- tests/graphql/invoices.rs
+++ tests/graphql/invoices.rs
@@ -55,6 +55,7 @@ mod graphql {
     }
 
     #[allow(dead_code)]
+    #[allow(non_snake_case)]
     #[derive(Serialize)]
     struct InvoicePricing {
         totalAfterTax: f64,
@@ -62,6 +63,7 @@ mod graphql {
 
     fn build_invoices_query() -> Operation<'static, InvoicesRoot> {
         let select_invoice_pricing: SelectionSet<'_, InvoicePricing, InvoicePricing> = map1(
+            #[allow(non_snake_case)]
             |totalAfterTax| InvoicePricing { totalAfterTax },
             field("totalAfterTax", vec![], float()),
         );
@@ -106,22 +108,28 @@ mod graphql {
         let mock_stocks: Vec<StockLineRow> = mock_stock_lines();
         let mock_invoices: Vec<InvoiceRow> = mock_invoices();
         let mock_invoice_lines: Vec<InvoiceLineRow> = mock_invoice_lines();
-        for name in mock_names {
+
+        for name in &mock_names {
             name_repository.insert_one(&name).await.unwrap();
         }
-        for store in mock_stores {
+
+        for store in &mock_stores {
             store_repository.insert_one(&store).await.unwrap();
         }
-        for item in mock_items {
+
+        for item in &mock_items {
             item_repository.insert_one(&item).await.unwrap();
         }
-        for stock_line in mock_stocks {
+
+        for stock_line in &mock_stocks {
             stock_repository.insert_one(&stock_line).await.unwrap();
         }
-        for invoice in mock_invoices {
+
+        for invoice in &mock_invoices {
             invoice_repository.insert_one(&invoice).await.unwrap();
         }
-        for invoice_line in mock_invoice_lines {
+
+        for invoice_line in &mock_invoice_lines {
             invoice_line_repository
                 .insert_one(&invoice_line)
                 .await
@@ -151,37 +159,33 @@ mod graphql {
             .to_request();
 
         let response = actix_web::test::read_response(&mut app, request).await;
-        let result = String::from_utf8(response.to_vec()).expect("Failed to parse response");
+        let result = String::from_utf8(response.to_vec()).expect("Failed to parse result");
 
         let expected = serde_json::to_string(&InvoicesResult {
             data: InvoicesRoot {
                 invoices: InvoicesConnection {
-                    nodes: vec![
-                        Invoice {
-                            id: "customer_invoice_a".to_owned(),
-                            pricing: InvoicePricing { totalAfterTax: 3.0 },
-                        },
-                        Invoice {
-                            id: "customer_invoice_b".to_owned(),
-                            pricing: InvoicePricing { totalAfterTax: 7.0 },
-                        },
-                        Invoice {
-                            id: "supplier_invoice_a".to_owned(),
+                    nodes: (&mock_invoices)
+                        .into_iter()
+                        .map(|invoice| Invoice {
+                            id: invoice.id.to_owned(),
                             pricing: InvoicePricing {
-                                totalAfterTax: 11.0,
+                                totalAfterTax: (&mock_invoice_lines).into_iter().fold(
+                                    0.0,
+                                    |acc, invoice_line| {
+                                        if invoice_line.invoice_id == invoice.id {
+                                            acc + invoice_line.total_after_tax
+                                        } else {
+                                            acc
+                                        }
+                                    },
+                                ),
                             },
-                        },
-                        Invoice {
-                            id: "supplier_invoice_b".to_owned(),
-                            pricing: InvoicePricing {
-                                totalAfterTax: 15.0,
-                            },
-                        },
-                    ],
+                        })
+                        .collect(),
                 },
             },
         })
-        .expect("Failed to parse result");
+        .unwrap();
 
         assert_eq!(result, expected);
     }
