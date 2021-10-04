0dbdd7371ca14a38644cf4674bea2de5c1b6f0c1
wlthomson <will.tho@outlook.com>
2021-10-04 15:06:57 +1300

    Refactor requisition gql test
    Refactors requisition gql test to use `cynic` and `serde_json` for
    constructing queries and expected results.

--- tests/graphql/requisition.rs
+++ tests/graphql/requisition.rs
@@ -17,6 +17,46 @@ mod graphql {
         util::test_db,
     };
 
+    use cynic::{
+        selection_set::{field, map1, string},
+        Argument, Operation, QueryRoot, SelectionSet,
+    };
+
+    use serde::Serialize;
+
+    #[derive(Serialize)]
+    struct RequisitionResult {
+        data: RequisitionRoot,
+    }
+
+    #[derive(Serialize)]
+    struct RequisitionRoot {
+        requisition: Requisition,
+    }
+
+    impl QueryRoot for RequisitionRoot {}
+
+    #[derive(Serialize)]
+    struct Requisition {
+        id: String,
+    }
+
+    fn build_requisition_query(id: &str) -> Operation<'static, RequisitionRoot> {
+        let select_requisition: SelectionSet<'_, Requisition, Requisition> =
+            map1(|id| Requisition { id }, field("id", vec![], string()));
+
+        let select_requisition_root: SelectionSet<'_, RequisitionRoot, RequisitionRoot> = map1(
+            |requisition| RequisitionRoot { requisition },
+            field(
+                "requisition",
+                vec![Argument::new("id", "String", id)],
+                select_requisition,
+            ),
+        );
+
+        Operation::query(select_requisition_root)
+    }
+
     #[actix_rt::test]
     async fn get_requisition_by_id_is_success() {
         let mock_names: Vec<NameRow> = mock_names();
@@ -35,15 +75,15 @@ mod graphql {
         let store_repository = repositories.get::<StoreRepository>().unwrap();
         let requisition_repository = repositories.get::<RequisitionRepository>().unwrap();
 
-        for name in mock_names {
+        for name in &mock_names {
             name_repository.insert_one(&name).await.unwrap();
         }
 
-        for store in mock_stores {
+        for store in &mock_stores {
             store_repository.insert_one(&store).await.unwrap();
         }
 
-        for requisition in mock_requisitions {
+        for requisition in &mock_requisitions {
             requisition_repository
                 .insert_one(&requisition)
                 .await
@@ -64,18 +104,30 @@ mod graphql {
         )
         .await;
 
-        // TODO: parameterise gql test payloads and expected results.
-        let payload = r#"{"query":"{requisition(id:\"requisition_a\"){id}}"}"#.as_bytes();
+        let requisition_id = mock_requisitions
+            .first()
+            .expect("Failed to find mock requisition")
+            .id
+            .to_owned();
 
-        let req = actix_web::test::TestRequest::post()
+        let requisition_query = build_requisition_query(&requisition_id);
+
+        let request = actix_web::test::TestRequest::post()
             .header("content-type", "application/json")
-            .set_payload(payload)
+            .set_json(&requisition_query)
             .uri("/graphql")
             .to_request();
 
-        let res = actix_web::test::read_response(&mut app, req).await;
-        let body = String::from_utf8(res.to_vec()).expect("Failed to parse response");
+        let response = actix_web::test::read_response(&mut app, request).await;
+        let result = String::from_utf8(response.to_vec()).expect("Failed to parse response");
 
-        assert_eq!(body, r#"{"data":{"requisition":{"id":"requisition_a"}}}"#);
+        let expected = serde_json::to_string(&RequisitionResult {
+            data: RequisitionRoot {
+                requisition: Requisition { id: requisition_id },
+            },
+        })
+        .unwrap();
+
+        assert_eq!(result, expected);
     }
 }
