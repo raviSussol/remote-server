5a1b76ce86ebbe7b3e189626272e9a5319d3756b
andreievg <andrei.evguenov@gmail.com>
2021-10-22 16:41:47 +1300

    type

--- src/server/service/graphql/schema/mutations/mod.rs
+++ src/server/service/graphql/schema/mutations/mod.rs
@@ -22,10 +22,109 @@ use async_graphql::*;
 use customer_invoice::*;
 use supplier_invoice::*;
 
+#[derive(SimpleObject)]
+#[graphql(concrete(
+    name = "InsertCustomerInvoiceResponseWithId",
+    params(InsertCustomerInvoiceResponse)
+))]
+#[graphql(concrete(
+    name = "InsertSupplierInvoiceResponseWithId",
+    params(InsertSupplierInvoiceResponse)
+))]
+#[graphql(concrete(
+    name = "UpdateCustomerInvoiceResponseWithId",
+    params(UpdateCustomerInvoiceResponse)
+))]
+#[graphql(concrete(
+    name = "UpdateSupplierInvoiceResponseWithId",
+    params(UpdateSupplierInvoiceResponse)
+))]
+#[graphql(concrete(
+    name = "DeleteCustomerInvoiceResponseWithId",
+    params(DeleteCustomerInvoiceResponse)
+))]
+#[graphql(concrete(
+    name = "DeleteSupplierInvoiceResponseWithId",
+    params(DeleteSupplierInvoiceResponse)
+))]
+#[graphql(concrete(
+    name = "InsertCustomerInvoiceLineResponseWithId",
+    params(InsertCustomerInvoiceLineResponse)
+))]
+#[graphql(concrete(
+    name = "InsertSupplierInvoiceLineResponseWithId",
+    params(InsertSupplierInvoiceLineResponse)
+))]
+#[graphql(concrete(
+    name = "UpdateCustomerInvoiceLineResponseWithId",
+    params(UpdateCustomerInvoiceLineResponse)
+))]
+#[graphql(concrete(
+    name = "UpdateSupplierInvoiceLineResponseWithId",
+    params(UpdateSupplierInvoiceLineResponse)
+))]
+#[graphql(concrete(
+    name = "DeleteCustomerInvoiceLineResponseWithId",
+    params(DeleteCustomerInvoiceLineResponse)
+))]
+#[graphql(concrete(
+    name = "DeleteSupplierInvoiceLineResponseWithId",
+    params(DeleteSupplierInvoiceLineResponse)
+))]
+pub struct MutationWithId<T: OutputType> {
+    pub id: String,
+    pub result: T,
+}
+
+#[derive(SimpleObject)]
+pub struct AllMutations {
+    insertCustomerInvoice: Option<Vec<MutationWithId<InsertCustomerInvoiceResponse>>>,
+    insertSupplierInvoice: Option<Vec<MutationWithId<InsertSupplierInvoiceResponse>>>,
+
+    insertCustomerInvoiceLine: Option<Vec<MutationWithId<InsertCustomerInvoiceLineResponse>>>,
+    insertSupplierInvoiceLine: Option<Vec<MutationWithId<InsertSupplierInvoiceLineResponse>>>,
+
+    updateCustomerInvoiceLine: Option<Vec<MutationWithId<UpdateCustomerInvoiceLineResponse>>>,
+    updateSupplierInvoiceLine: Option<Vec<MutationWithId<UpdateSupplierInvoiceLineResponse>>>,
+
+    deleteCustomerInvoiceLine: Option<Vec<MutationWithId<DeleteCustomerInvoiceLineResponse>>>,
+    deleteSupplierInvoiceLine: Option<Vec<MutationWithId<DeleteSupplierInvoiceLineResponse>>>,
+
+    updateCustomerInvoice: Option<Vec<MutationWithId<UpdateCustomerInvoiceResponse>>>,
+    updateSupplierInvoice: Option<Vec<MutationWithId<UpdateSupplierInvoiceResponse>>>,
+
+    deleteCustomerInvoice: Option<Vec<MutationWithId<DeleteCustomerInvoiceResponse>>>,
+    deleteSupplierInvoice: Option<Vec<MutationWithId<DeleteSupplierInvoiceResponse>>>,
+}
+
 pub struct Mutations;
 
 #[Object]
 impl Mutations {
+    async fn all(
+        &self,
+        ctx: &Context<'_>,
+        insertCustomerInvoice: Option<Vec<InsertCustomerInvoiceInput>>,
+        insertSupplierInvoice: Option<Vec<InsertSupplierInvoiceInput>>,
+
+        insertSupplierInvoiceLine: Option<Vec<InsertSupplierInvoiceLineInput>>,
+        insertCustomerInvoiceLine: Option<Vec<InsertCustomerInvoiceLineInput>>,
+
+        updateCustomerInvoiceLine: Option<Vec<UpdateCustomerInvoiceLineInput>>,
+        updateSupplierInvoiceLine: Option<Vec<UpdateSupplierInvoiceLineInput>>,
+
+        deleteCustomerInvoiceLine: Option<Vec<DeleteCustomerInvoiceLineInput>>,
+        deleteSupplierInvoiceLine: Option<Vec<DeleteSupplierInvoiceLineInput>>,
+
+        updateCustomerInvoice: Option<Vec<InsertCustomerInvoiceInput>>,
+        updateSupplierInvoice: Option<Vec<InsertSupplierInvoiceInput>>,
+
+        deleteCustomerInvoice: Option<Vec<String>>,
+        deleteSupplierInvoice: Option<Vec<DeleteSupplierInvoiceInput>>,
+    ) -> AllMutations {
+        todo!();
+    }
+
     async fn insert_customer_invoice(
         &self,
         ctx: &Context<'_>,
